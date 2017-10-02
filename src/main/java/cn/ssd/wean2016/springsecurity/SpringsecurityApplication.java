63d9489c930fa1398c086cb710d0560f4817ae12
wean <weanyq@gmail.com>
2017-10-02 20:27:48 +0800

    步骤一 ： 建立项目，配置好数据源

---
+++ src/main/java/cn/ssd/wean2016/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,12 @@
+package cn.ssd.wean2016.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/ssd/wean2016/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.ssd.wean2016.springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringsecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
