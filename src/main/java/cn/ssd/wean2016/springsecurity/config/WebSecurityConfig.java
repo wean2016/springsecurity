7842ff5663ce4559df0518a832354649ba8be0ea
wean <weanyq@gmail.com>
2017-10-05 11:05:03 +0800

    注册步骤四的拦截器，使它在 Spring Security 读取本次会话权限前将用户所具有的权限写入本次会话中

--- src/main/java/cn/ssd/wean2016/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/cn/ssd/wean2016/springsecurity/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package cn.ssd.wean2016.springsecurity.config;
 
+import cn.ssd.wean2016.springsecurity.filter.AuthenticationTokenFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @version V1.0.0
@@ -16,15 +19,41 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity      // 启用 Spring Security web 安全的功能
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 注册 token 转换拦截器为 bean
+     * 如果客户端传来了 token ，那么通过拦截器解析 token 赋予用户权限
+     *
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+        AuthenticationTokenFilter authenticationTokenFilter = new AuthenticationTokenFilter();
+        authenticationTokenFilter.setAuthenticationManager(authenticationManagerBean());
+        return authenticationTokenFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/auth").authenticated()       // 需携带有效 token
+                .antMatchers("/admin").hasAuthority("admin")   // 需拥有 admin 这个权限
+                .antMatchers("/ADMIN").hasRole("ADMIN")     // 需拥有 ADMIN 这个身份
                 .anyRequest().permitAll()       // 允许所有请求通过
                 .and()
                 .csrf()
                 .disable()                      // 禁用 Spring Security 自带的跨域处理
                 .sessionManagement()                        // 定制我们自己的 session 策略
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 调整为让 Spring Security 不创建和使用 session
+
+
+        /**
+         * 本次 json web token 权限控制的核心配置部分
+         * 在 Spring Security 开始判断本次会话是否有权限时的前一瞬间
+         * 通过添加过滤器将 token 解析，将用户所有的权限写入本次会话
+         */
+        http
+                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
     }
 }
