aa984715dc8dda2b4409ffc416d17c19ced20c43
wean <weanyq@gmail.com>
2017-10-05 10:18:49 +0800

    定义解析 token 的拦截器

---
+++ src/main/java/cn/ssd/wean2016/springsecurity/filter/AuthenticationTokenFilter.java
@@ -0,0 +1,88 @@
+package cn.ssd.wean2016.springsecurity.filter;
+
+import cn.ssd.wean2016.springsecurity.utils.TokenUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
+/**
+ * @version V1.0.0
+ * @Description 解析 token 的过滤器
+ * 配置在 Spring Security 的配置类中
+ * 用于解析 token ，将用户所有的权限写入本次 Spring Security 的会话中
+ * @Author liuyuequn weanyq@gmail.com
+ * @Date 2017年8月11日10:59:57
+ */
+public class AuthenticationTokenFilter extends UsernamePasswordAuthenticationFilter {
+
+    /**
+     * json web token 在请求头的名字
+     */
+    @Value("${token.header}")
+    private String tokenHeader;
+
+    /**
+     * 辅助操作 token 的工具类
+     */
+    @Autowired
+    private TokenUtils tokenUtils;
+
+    /**
+     * Spring Security 的核心操作服务类
+     * 在当前类中将使用 UserDetailsService 来获取 userDetails 对象
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // 将 ServletRequest 转换为 HttpServletRequest 才能拿到请求头中的 token
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        // 尝试获取请求头的 token
+        String authToken = httpRequest.getHeader(this.tokenHeader);
+        // 尝试拿 token 中的 username
+        // 若是没有 token 或者拿 username 时出现异常，那么 username 为 null
+        String username = this.tokenUtils.getUsernameFromToken(authToken);
+
+        // 如果上面解析 token 成功并且拿到了 username 并且本次会话的权限还未被写入
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // 用 UserDetailsService 从数据库中拿到用户的 UserDetails 类
+            // UserDetails 类是 Spring Security 用于保存用户权限的实体类
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            // 检查用户带来的 token 是否有效
+            // 包括 token 和 userDetails 中用户名是否一样， token 是否过期， token 生成时间是否在最后一次密码修改时间之前
+            // 若是检查通过
+            if (this.tokenUtils.validateToken(authToken, userDetails)) {
+                // 生成通过认证
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+                // 将权限写入本次会话
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            if (!userDetails.isEnabled()){
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().print("{\"code\":\"452\",\"data\":\"\",\"message\":\"账号处于黑名单\"}");
+                return;
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
--- src/main/java/cn/ssd/wean2016/springsecurity/utils/TokenUtils.java
+++ src/main/java/cn/ssd/wean2016/springsecurity/utils/TokenUtils.java
@@ -1,11 +1,13 @@
 package cn.ssd.wean2016.springsecurity.utils;
 
 import cn.ssd.wean2016.springsecurity.model.TokenDetail;
+import cn.ssd.wean2016.springsecurity.model.UserDetailImpl;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
@@ -121,4 +123,68 @@ public class TokenUtils {
         return claims;
     }
 
+    /**
+     * 检查 token 是否处于有效期内
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        UserDetailImpl user = (UserDetailImpl) userDetails;
+        final String username = this.getUsernameFromToken(token);
+        final Date created = this.getCreatedDateFromToken(token);
+        return (username.equals(user.getUsername()) && !(this.isTokenExpired(token)) && !(this.isCreatedBeforeLastPasswordReset(created, user.getLastPasswordReset())));
+    }
+
+    /**
+     * 获得我们封装在 token 中的 token 创建时间
+     * @param token
+     * @return
+     */
+    public Date getCreatedDateFromToken(String token) {
+        Date created;
+        try {
+            final Claims claims = this.getClaimsFromToken(token);
+            created = new Date((Long) claims.get("created"));
+        } catch (Exception e) {
+            created = null;
+        }
+        return created;
+    }
+
+    /**
+     * 获得我们封装在 token 中的 token 过期时间
+     * @param token
+     * @return
+     */
+    public Date getExpirationDateFromToken(String token) {
+        Date expiration;
+        try {
+            final Claims claims = this.getClaimsFromToken(token);
+            expiration = claims.getExpiration();
+        } catch (Exception e) {
+            expiration = null;
+        }
+        return expiration;
+    }
+
+    /**
+     * 检查当前时间是否在封装在 token 中的过期时间之后，若是，则判定为 token 过期
+     * @param token
+     * @return
+     */
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = this.getExpirationDateFromToken(token);
+        return expiration.before(this.generateCurrentDate());
+    }
+
+    /**
+     * 检查 token 是否是在最后一次修改密码之前创建的（账号修改密码之后之前生成的 token 即使没过期也判断为无效）
+     * @param created
+     * @param lastPasswordReset
+     * @return
+     */
+    private Boolean isCreatedBeforeLastPasswordReset(Date created, Date lastPasswordReset) {
+        return (lastPasswordReset != null && created.before(lastPasswordReset));
+    }
 }
